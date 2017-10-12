1cc6e924f390ba84d43777fb45f3929ff5e5f456
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2017-10-12 13:09:18 -0400

    make shit happen

---
+++ src/FitBit/FileIO.java
@@ -0,0 +1,19 @@
+package FitBit;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * FileIO.java
+ * AUTHOR: TEAM HUNGRY
+ * CLASS: CSCI 360-01
+ * PURPOSE: TO READ THE SPECIFIED FILE TO A USABLE FORMAT
+ *      TO OVERWRITE THE SPECIFIED FILE WITH REQUESTED CHANGES
+ *  OTHER: USED TO GENERATE AND UPDATE THREE FILES
+ *      1) RAW DATA DATASET
+ *      2) SETTINGS DATASET
+ *      3) DISPLAY READY DATASET
+ */
+public class FileIO {
+}
