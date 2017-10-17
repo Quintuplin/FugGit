812f41bfd5f35145fcad1e349af9aa80c7889b6c
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2017-10-17 09:46:33 -0400

    some progress on organizing and labeling classes

--- src/fitbit/Display.java
+++ src/fitbit/Display.java
@@ -14,5 +14,42 @@ package fitbit;
  *      RDD: CALLS INFORMATION EXPERT
  */
 public class Display {
-    //TEST THIS SHIT;
+    static int menu = 0;
+    static int menub = 0;
+    public static void main(String[] args) {
+        initScreen(); //starts the simulation
+        while(true) {
+            dataDisplay(); //displays the currently selected menu
+            buttonListen(); //listens for button presses
+        }
+
+    }
+    //output data to display
+    private static void dataDisplay(){
+        if (menu ==0){
+            //display menu 1; clock, heartrate, steps?
+        }
+        else if (menu == 1){
+            //display menu 1;
+        }
+        //goes on for each menu
+    }
+
+    //listen for button press
+    private static void buttonListen(){
+        if (){//button1press
+            menu= menu++%4;
+            menub = 0;
+        }
+        if (){//button2press
+            if(button2hold){//button2hold placeholder if statement
+                //enter edit mode
+            }else{//button2tap
+                //alter secondary menu
+                menub++:
+            }
+        }
+    }
+        //if button press, alter menu
+
 }
