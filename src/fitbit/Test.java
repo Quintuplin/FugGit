841bdc3633367b9f14812d30a7cbc7936e4f731f
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2017-11-21 08:48:23 -0500

    improvement to test
    now it compiles

--- src/fitbit/Test.java
+++ src/fitbit/Test.java
@@ -4,12 +4,12 @@ public class Test {
 
     private static void TestClock () {
         try {
-            System.out.println(time());
+            System.out.println(Clock.time());
             Thread.sleep(1000);
-            System.out.println(time());
-            System.out.println(time());
-            System.out.println(time());
-            System.out.println(time());
+            System.out.println(Clock.time());
+            System.out.println(Clock.time());
+            System.out.println(Clock.time());
+            System.out.println(Clock.time());
         } catch (java.lang.InterruptedException e) {
             System.out.println("Interrupted");
         }
