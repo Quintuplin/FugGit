5d2f17906c1a9230d6d16a15ace8ad4e473d834a
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2017-11-27 14:43:38 -0500

    Has basic window
    Will improve to add buttons, organization, and data

--- src/fitbit/DisplayWindow.java
+++ src/fitbit/DisplayWindow.java
@@ -2,7 +2,7 @@ package fitbit;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class DisplayWindow implements ActionListener{
 
@@ -17,18 +17,18 @@ public class DisplayWindow implements ActionListener{
 
     public DisplayWindow(){
         //create window
-        displayFrame = new Jframe("FITBIT DEMO");
+        displayFrame = new JFrame("FITBIT DEMO");
         displayFrame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
-        displayFrame.setSize(600,600);
+        displayFrame.setSize(new Dimension(600,600));
 
         //create and set up panel
-        displayPanel = new JPanel((new GridLayout(6,6)));
+        displayPanel = new JPanel(new GridLayout(6,6));
 
         //add widgets
         addWidgets();
 
         //set default button?
-        displayFrame.getRootPane().setDefaultButton((frontButton));
+        displayFrame.getRootPane().setDefaultButton(frontButton);
 
         //add panel to window
         displayFrame.getContentPane().add(displayPanel, BorderLayout.CENTER);
@@ -40,8 +40,59 @@ public class DisplayWindow implements ActionListener{
 
 
     private void addWidgets(){
-        //create widgets
-        WORKING ON IT
+
+        //labels
+        timeHours = new JLabel("Hours", SwingConstants.LEFT);
+        timeMinutes = new JLabel("Minutes", SwingConstants.LEFT);
+        timeSeconds = new JLabel("Seconds", SwingConstants.LEFT);
+        dateMonths = new JLabel("Months", SwingConstants.LEFT);
+        dateDays = new JLabel("Days", SwingConstants.LEFT);
+        dateYears = new JLabel("Years", SwingConstants.LEFT);
+        heartrate = new JLabel("Heart Rate", SwingConstants.LEFT);
+        steps = new JLabel("Steps", SwingConstants.LEFT);
+        activity = new JLabel("Activity", SwingConstants.LEFT);
+        caloriesBurned = new JLabel("Calories Burned", SwingConstants.LEFT);
+
+        //buttons
+        sideButton = new JButton("Side Button");
+        frontButton = new JButton("Front Button");
+        senseStep = new JButton("Sense Step");
+        senseBeat = new JButton("Sense Beat");
+        sideButton.addActionListener(this);
+        frontButton.addActionListener(this);
+        senseStep.addActionListener(this);
+        senseBeat.addActionListener(this);
+
+        //pop it all to the container
+        displayPanel.add(sideButton);
+        displayPanel.add(frontButton);
+        displayPanel.add(timeHours);
+        displayPanel.add(timeMinutes);
+        displayPanel.add(timeSeconds);
+        displayPanel.add(dateDays);
+        displayPanel.add(dateMonths);
+        displayPanel.add(dateYears);
+        displayPanel.add(heartrate);
+        displayPanel.add(steps);
+        displayPanel.add(activity);
+        displayPanel.add(caloriesBurned);
     }
 
+    public void actionPerformed(ActionEvent event){
+        //change menu
+    }
+
+    private static void showGUI(){
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        DisplayWindow window = new DisplayWindow();
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                showGUI();
+            }
+        });
+    }
 }
