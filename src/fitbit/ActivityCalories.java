92500d76062dbd72059a14e1012c5fa36391d7eb
Quintuplin <quintuplin12@gmail.com>
2017-11-28 01:24:29 -0500

    improved calorie and activity calculations significantly

--- src/fitbit/ActivityCalories.java
+++ src/fitbit/ActivityCalories.java
@@ -14,6 +14,8 @@ public class ActivityCalories {
     float aBPM = 0;
     int i = 0;
 
+    private Clock clock = new Clock();
+
     //finds average BPM for current day
     private double averageBPM() {
     //    aBPM += StepsHeartrate.getBPM() - aBPM / i++;
@@ -24,10 +26,10 @@ public class ActivityCalories {
     private double calculateCalories(int age, double weight, boolean isMale, int BPM, double time){
         if (isMale){
             double V02Max = ((-55.0969 + (.6309 * BPM) + (.1988 * weight) + (.2017 * age)) / 4.184) * 60 * time;
-            return ((-95.7735 + (.634 * BPM) + (.404 * V02Max) + (.394 * weight) + (.271 * age)) / 4.184) * 60 * time;
+            return -((-95.7735 + (.634 * BPM) + (.404 * V02Max) + (.394 * weight) + (.271 * age)) / 4.184) * 60 * time;
         } else{ //if female
             double V02Max = ((-20.4022 + (.4472 * BPM) + (.1263 * weight) + (.074 * age)) / 4.184) * 60 * time;
-            return ((-59.3954 + (.45 * BPM) + (.380 * V02Max) + (.103 * weight) + (.274 * age)) / 4.184) * 60 * time;
+            return -((-59.3954 + (.45 * BPM) + (.380 * V02Max) + (.103 * weight) + (.274 * age)) / 4.184) * 60 * time;
         }
     }
 
@@ -36,14 +38,16 @@ public class ActivityCalories {
         int age = 25;
         int weight = 155;
         boolean isMale = true;
-        double time = 10000.0;
+        double time = Double.parseDouble(clock.currentTime().substring(0,2))
+                + Double.parseDouble(clock.currentTime().substring(3,5)) / 60
+                + Double.parseDouble(clock.currentTime().substring(6,8)) / (60 * 60);
         caloriesBurned = calculateCalories(age, weight, isMale, (int)aBPM, time);
         return caloriesBurned;
     }
 
     public float getActivity(){
-        if (Sensors.BPSCheat() > 60)
-            activity += .25;
+        if (Sensors.BPSCheat() > 100)
+            activity += (.01);
         return activity;
     }
 }
